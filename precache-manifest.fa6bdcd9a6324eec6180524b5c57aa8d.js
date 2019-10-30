e6c14638cbaa0987386081fe02fdcd420a838a3a
Batgerelt <32481226+batgrerelt@users.noreply.github.com>
2019-10-30 15:00:22 +0800

    asd

---
+++ precache-manifest.fa6bdcd9a6324eec6180524b5c57aa8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66ca924936fc3b5db9c32f4dcfd56df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a1ed56351f7dceb1fff",
+    "url": "/static/css/main.ef40a8b4.chunk.css"
+  },
+  {
+    "revision": "b98f0530cd49a4d2d3c4",
+    "url": "/static/js/2.a5bc0d7c.chunk.js"
+  },
+  {
+    "revision": "6a1ed56351f7dceb1fff",
+    "url": "/static/js/main.9cff050e.chunk.js"
+  },
+  {
+    "revision": "6ea5e99dff7b55c07ef0",
+    "url": "/static/js/runtime-main.0c42428d.js"
+  },
+  {
+    "revision": "802dfba08379a818a6231bf95cc5b872",
+    "url": "/static/media/1.802dfba0.jpg"
+  },
+  {
+    "revision": "55444ca1b195e9c411a6aebaf4049613",
+    "url": "/static/media/2.55444ca1.jpg"
+  },
+  {
+    "revision": "f8482a889539a4225e83716ef9e4653f",
+    "url": "/static/media/slider.f8482a88.jpg"
+  }
+]);
